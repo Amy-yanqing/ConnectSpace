a36824696b7d56cfb944319a78c9d846ff3428e8
Amy-yanqing <113411770+Amy-yanqing@users.noreply.github.com>
2025-05-03 21:35:02 +1000

    4-prepared our app for deployment

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import userRoutes from "./routes/user.route.js"
 import connectMongoDB from "./db/connectMongoDB.js"
 import notificationRoutes from "./routes/notification.route.js";
 import postRoutes from "./routes/post.route.js";
+import path from "path";
 
 dotenv.config();
 cloudinary.config({
@@ -21,6 +22,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json({limit:"6mb"}));
@@ -29,6 +31,11 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/notifications",notificationRoutes);
 app.use("/api/posts",postRoutes);
+
+if (process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.get("/{*any}",(req,res)=>{res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))})
+}
 
 
 app.listen(PORT,()=>{
